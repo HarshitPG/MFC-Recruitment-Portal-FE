dbd39f979c6a212dcbf17099de88438e41cea039
Keshav-Aneja <keshav.aneja09@gmail.com>
2024-04-15 13:55:39 +0530

    fix: deployment errors

--- src/component3d/Fox.tsx
+++ src/component3d/Fox.tsx
@@ -11,8 +11,8 @@ const Fox = (props: JSX.IntrinsicElements["group"]) => {
     window.open(url, "_blank");
   };
   const texture = useLoader(TextureLoader, "../../public/Dino.png");
-  const group = useRef<THREE.Group>();
-  const meshRef = useRef<THREE.Mesh>();
+  const group = useRef<THREE.Group>(null);
+  const meshRef = useRef<THREE.Mesh>(null);
   const { nodes, materials, animations } = useGLTF(
     "../../public/model/fox1.gltf"
   );
@@ -24,7 +24,7 @@ const Fox = (props: JSX.IntrinsicElements["group"]) => {
 
   useEffect(() => {
     setAnimations(names);
-  }, [names]);
+  }, [names, setAnimations]);
   useEffect(() => {
     if (actions && names) {
       actions[names[animationIndex]]?.reset().fadeIn(0.5).play();
@@ -34,7 +34,7 @@ const Fox = (props: JSX.IntrinsicElements["group"]) => {
         actions[names[animationIndex]]?.fadeOut(0.5);
       }
     };
-  }, [animationIndex]);
+  }, [animationIndex, actions, names]);
 
   useEffect(() => {
     if (group.current && animationIndex === 7) {
@@ -42,14 +42,19 @@ const Fox = (props: JSX.IntrinsicElements["group"]) => {
         y: Math.PI / 2,
         z: -Math.PI / 2,
         x: Math.PI / 2,
-
         duration: 0,
       });
       gsap.to(group.current.position, {
         x: 15,
         duration: 3.5,
         onComplete: () => {
-          gsap.to(group.current.rotation, { y: 0, z: 0, x: 0, duration: 0.5 });
+          if (group.current)
+            gsap.to(group.current.rotation, {
+              y: 0,
+              z: 0,
+              x: 0,
+              duration: 0.5,
+            });
           setTimeout(() => {
             setAnimationIndex(2);
           }, 150);
@@ -65,13 +70,12 @@ const Fox = (props: JSX.IntrinsicElements["group"]) => {
 
           return () => {
             clearTimeout(timeout);
-
             actions[names[animationIndex]]?.fadeOut(0.5);
           };
         },
       });
     }
-  }, [animationIndex]);
+  }, [animationIndex, actions, names, setAnimationIndex]);
 
   useEffect(() => {
     if (group.current && animationIndex === 5) {
@@ -79,21 +83,27 @@ const Fox = (props: JSX.IntrinsicElements["group"]) => {
         y: Math.PI / 2,
         z: -Math.PI / 2,
         x: Math.PI / 2,
-
         duration: 0,
       });
       gsap.to(group.current.position, {
         x: 12.5,
         duration: 1,
         onComplete: () => {
-          gsap.to(group.current.rotation, { y: 0, z: 0, x: 0, duration: 0.5 });
+          if (group.current)
+            gsap.to(group.current.rotation, {
+              y: 0,
+              z: 0,
+              x: 0,
+              duration: 0.5,
+            });
           setTimeout(() => {
             setAnimationIndex(4);
-            gsap.to(meshRef.current.position, {
-              y: 5,
-              duration: 3.5,
-              ease: "power1.out",
-            });
+            if (meshRef.current)
+              gsap.to(meshRef.current.position, {
+                y: 5,
+                duration: 3.5,
+                ease: "power1.out",
+              });
           }, 150);
 
           actions[names[animationIndex]]?.play();
@@ -108,13 +118,12 @@ const Fox = (props: JSX.IntrinsicElements["group"]) => {
 
           return () => {
             clearTimeout(timeout);
-
             actions[names[animationIndex]]?.fadeOut(0.5);
           };
         },
       });
     }
-  }, [animationIndex]);
+  }, [animationIndex, actions, names, setAnimationIndex]);
 
   useEffect(() => {
     if (animationIndex === 2) {
@@ -128,19 +137,17 @@ const Fox = (props: JSX.IntrinsicElements["group"]) => {
     return () => {
       actions[names[animationIndex]]?.fadeOut(0.5);
     };
-  }, [animationIndex]);
+  }, [animationIndex, actions, names, setAnimationIndex]);
 
   return (
     <>
       <mesh
         ref={meshRef}
         position={[15, 15, 0]}
-        transparent
-        envMapIntensity={2}
         onClick={() => redirectToURL("https://innovationx.mozillavit.in/")}
       >
-        <planeGeometry attach="geometry" args={[4, 4]} />
-        <meshStandardMaterial attach="material" map={texture} transparent />
+        <planeGeometry args={[4, 4]} />
+        <meshStandardMaterial map={texture} transparent />
       </mesh>
       <group ref={group} {...props} dispose={null}>
         <group name="Scene">
@@ -156,58 +163,58 @@ const Fox = (props: JSX.IntrinsicElements["group"]) => {
               <skinnedMesh
                 castShadow
                 name="Cube002_1"
-                geometry={nodes.Cube002_1.geometry}
+                geometry={(nodes.Cube002_1 as THREE.Mesh).geometry}
                 material={materials.Base}
-                skeleton={nodes.Cube002_1.skeleton}
+                skeleton={(nodes.Cube002_1 as THREE.SkinnedMesh).skeleton}
               />
               <skinnedMesh
                 castShadow
                 name="Cube002_2"
-                geometry={nodes.Cube002_2.geometry}
+                geometry={(nodes.Cube002_2 as THREE.Mesh).geometry}
                 material={materials["Material.001"]}
-                skeleton={nodes.Cube002_2.skeleton}
+                skeleton={(nodes.Cube002_2 as THREE.SkinnedMesh).skeleton}
               />
               <skinnedMesh
                 castShadow
                 name="Cube002_3"
-                geometry={nodes.Cube002_3.geometry}
+                geometry={(nodes.Cube002_3 as THREE.Mesh).geometry}
                 material={materials["Material.003"]}
-                skeleton={nodes.Cube002_3.skeleton}
+                skeleton={(nodes.Cube002_3 as THREE.SkinnedMesh).skeleton}
               />
               <skinnedMesh
                 castShadow
                 name="Cube002_4"
-                geometry={nodes.Cube002_4.geometry}
+                geometry={(nodes.Cube002_4 as THREE.Mesh).geometry}
                 material={materials["Material.002"]}
-                skeleton={nodes.Cube002_4.skeleton}
+                skeleton={(nodes.Cube002_4 as THREE.SkinnedMesh).skeleton}
               />
               <skinnedMesh
                 castShadow
                 name="Cube002_5"
-                geometry={nodes.Cube002_5.geometry}
+                geometry={(nodes.Cube002_5 as THREE.Mesh).geometry}
                 material={materials.Mouth}
-                skeleton={nodes.Cube002_5.skeleton}
+                skeleton={(nodes.Cube002_5 as THREE.SkinnedMesh).skeleton}
               />
               <skinnedMesh
                 castShadow
                 name="Cube002_6"
-                geometry={nodes.Cube002_6.geometry}
+                geometry={(nodes.Cube002_6 as THREE.Mesh).geometry}
                 material={materials["Material.004"]}
-                skeleton={nodes.Cube002_6.skeleton}
+                skeleton={(nodes.Cube002_6 as THREE.SkinnedMesh).skeleton}
               />
               <skinnedMesh
                 castShadow
                 name="Cube002_7"
-                geometry={nodes.Cube002_7.geometry}
+                geometry={(nodes.Cube002_7 as THREE.Mesh).geometry}
                 material={materials["Material.005"]}
-                skeleton={nodes.Cube002_7.skeleton}
+                skeleton={(nodes.Cube002_7 as THREE.SkinnedMesh).skeleton}
               />
               <skinnedMesh
                 castShadow
                 name="Cube002_8"
-                geometry={nodes.Cube002_8.geometry}
+                geometry={(nodes.Cube002_8 as THREE.Mesh).geometry}
                 material={materials["Material.006"]}
-                skeleton={nodes.Cube002_8.skeleton}
+                skeleton={(nodes.Cube002_8 as THREE.SkinnedMesh).skeleton}
               />
             </group>
           </group>
