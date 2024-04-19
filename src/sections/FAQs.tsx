58e46b2608c6caade59e3a6fa192080043be3ff0
RAINAVINAYAK16 <vraina03@gmail.com>
2024-04-20 00:38:35 +0530

    chore : favicon and faq edit

--- src/sections/FAQs.tsx
+++ src/sections/FAQs.tsx
@@ -1,6 +1,6 @@
-import Navbar from "../components/Navbar";
 import BoundingBox from "../components/BoundingBox";
 import Faqcomp from "../components/Faqcomp";
+import Navbar from "../components/Navbar";
 const FAQs = () => {
   return (
     <div className="w-full h-full flex flex-col md:flex-row justify-center items-center p-4 md:p-12">
@@ -27,7 +27,7 @@ const FAQs = () => {
           />
           <Faqcomp
             que={"Is there a deadline for submitting applications?"}
-            ans={"Yes, one can apply for multiple domains."}
+            ans={"The deadline will be informed in advanced based on the domain you choose to apply in."}
           />
           <Faqcomp
             que={"How can I edit or update my application after submission?"}
