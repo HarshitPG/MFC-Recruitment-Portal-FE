467dac06baee331adb881124e5c2e547fb352290
Harshit P G <harshitpg2004@gmail.com>
2024-04-19 21:12:39 +0530

    routes fix

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,18 @@ import { CharacterAnimationsProvider } from "./context/CharAnimation.tsx";
 // import dotenv from "dotenv";
 // dotenv.config();
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <CharacterAnimationsProvider>
+//       <App />
+//     </CharacterAnimationsProvider>
+//   </React.StrictMode>
+// );
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(
+  <>
     <CharacterAnimationsProvider>
       <App />
     </CharacterAnimationsProvider>
-  </React.StrictMode>
+  </>
 );
