a4b762d7e4f0e3324b0d86fd19db2e59803a1505
Keshav-Aneja <keshav.aneja09@gmail.com>
2024-04-20 11:09:17 +0530

    feat: ganalytics added

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { CharacterAnimationsProvider } from "./context/CharAnimation.tsx";
+import ReactGA from "react-ga4";
 // import dotenv from "dotenv";
 // dotenv.config();
-
+ReactGA.initialize(`${import.meta.env.VITE_G_ANALYTICS}`);
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <CharacterAnimationsProvider>
