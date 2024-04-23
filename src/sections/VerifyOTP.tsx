42b024accea45e5127e0998713e9e608b69b99a9
Harshit P G <harshitpg2004@gmail.com>
2024-04-23 22:56:37 +0530

    minor resend otp fix

--- src/sections/VerifyOTP.tsx
+++ src/sections/VerifyOTP.tsx
@@ -85,13 +85,13 @@ const VerifyOTP: React.FC = () => {
     try {
       {
         const token = Cookies.get("jwtToken");
-        const userId = secureLocalStorage.getItem("id");
+        const id = secureLocalStorage.getItem("id");
         const email = secureLocalStorage.getItem("email");
         // console.log("userId:", userId);
         // console.log("email:", email);
 
         const response = await axios.post(
-          `${import.meta.env.VITE_BASE_URL}/auth/resendotp/${userId}`,
+          `${import.meta.env.VITE_BASE_URL}/auth/resendotp/${id}`,
           { email },
           {
             headers: {
